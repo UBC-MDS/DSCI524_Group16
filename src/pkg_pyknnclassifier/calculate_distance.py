eb192e06b8d8ef1a87cf3dbe5b0d685d7fbe4dea
Sho Inagaki <shoinagaki7@gmail.com>
2024-01-12 17:35:56 -0800

    Docstrings for calculate_distance

---
+++ src/pkg_pyknnclassifier/calculate_distance.py
@@ -0,0 +1,24 @@
+def calculate_distance(obs_1, obs_2):
+    """
+    This function calculates the Euclidean distance between two observations for the KNN model to find the similarity score.
+    
+    Parameters
+    ----------
+    obs_1 : array
+        An array containing the features of the first observation.
+    obs_2 : array
+        An array containing the features of the second observation.
+
+    Returns
+    -------
+    float
+        Float representing a euclidean distance between two observations.
+
+    Examples
+    --------
+    obs_1 = [0.81, 0.2, -0.86, 0.08]
+    obs_2 = [-0.39, 0.24, -0.77, 0.17]
+    dist = calculate_distance(obs_1, obs_2)
+    print(f"Euclidean Distance between two observations is {dist}")
+    """
+    pass
