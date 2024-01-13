dd52bad9149b9c265f8ab55c1e31de85bf48bc5f
weiranzhao97 <144717444+weiranzhao97@users.noreply.github.com>
2024-01-12 18:34:22 -0800

    Update data_loading.py

--- src/pkg_pyknnclassifier/data_loading.py
+++ src/pkg_pyknnclassifier/data_loading.py
@@ -1,16 +1,23 @@
 def data_loading(str_of_path):
+    """ Load data from a file path.
+
+    Parameters
+    ----------    
+    str_of_path: str 
+        The file path to the dataset.
+    
+    Returns
+    ---------- 
+    pd.DataFrame
+        The loaded data as a DataFrame.
+
+    Examples
+    --------
+    >>> # Loading from a CSV file
+    >>> df = data_loading('path/to/your/data.csv')
     """
-    Load data from a file path.
-
-    Args:
-    str_of_path (str): The file path to the dataset.
-
-    Returns:
-    pd.DataFrame: The loaded data as a DataFrame.
-
-    Example:
-    # Loading from a CSV file
-    df = data_loading('path/to/your/data.csv')
-    """
-    pass
+    if isinstance(str_of_path, str):
+        return pd.read_csv(str_of_path)
+    else:
+        raise ValueError("Input must be a file path or a DataFrame.")
 
