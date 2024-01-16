436afe7695522a4606b1697c7d74f924b7295fff
weiranzhao97 <144717444+weiranzhao97@users.noreply.github.com>
2024-01-16 15:48:15 -0800

    Update data_loading() with specifying target_column

--- src/pkg_pyknnclassifier/data_loading.py
+++ src/pkg_pyknnclassifier/data_loading.py
@@ -1,23 +1,28 @@
-def data_loading(str_of_path):
-    """ Load data from a file path.
+def data_loading(str_of_path, target_column):
+    """ Load data from a file path and split into features and target.
 
     Parameters
     ----------    
-    str_of_path: str 
+    str_of_path : str 
         The file path to the dataset.
+    target_column : str
+        The name of the column to be used as the target variable.
     
     Returns
     ---------- 
-    pd.DataFrame
-        The loaded data as a DataFrame.
+    pd.DataFrame, pd.Series
+        The features as a DataFrame and the target as a Series.
 
     Examples
     --------
-    >>> # Loading from a CSV file
-    >>> df = data_loading('path/to/your/data.csv')
+    >>> # Loading from a CSV file and selecting target column
+    >>> features, target = data_loading('path/to/your/data.csv', 'target_column_name')
     """
     if isinstance(str_of_path, str):
-        return pd.read_csv(str_of_path)
+        data = pd.read_csv(str_of_path)
+        train_y = data[target_column]
+        train_X = data.drop(columns=[target_column])
+        return train_X, train_y
     else:
-        raise ValueError("Input must be a file path or a DataFrame.")
+        raise ValueError("Input must be a string representing the file path.")
 
