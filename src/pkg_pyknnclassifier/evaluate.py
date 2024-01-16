49f45f5dc3cdc18eea803d7323cd2f0fd453df36
billwan96 <billwan@student.ubc.ca>
2024-01-16 15:21:32 -0800

    add evaluate function

--- src/pkg_pyknnclassifier/evaluate.py
+++ src/pkg_pyknnclassifier/evaluate.py
@@ -21,3 +21,34 @@ def evaluate(y_true, y_pred, metric='accuracy'):
     precision_result = evaluate_knn_manual(true_labels, predicted_labels, metric='precision')
     print("Precision:", precision_result)
     """
+    if metric == 'accuracy':
+        correct_pred = sum((y_pred == y_true).astype(int))
+        return correct_pred/ len(y_true)
+    
+    elif metric in ['precision', 'recall', 'f1']:
+        true_pos, pred_pos, real_pos = 0, 0, 0
+        precision, recall = 0, 0
+        for pred, true in zip(y_pred, y_true):
+            if pred == true == 1:
+                true_pos += 1
+            if pred == 1:
+                pred_pos += 1
+            if true == 1:
+                real_pos += 1
+        if pred_pos == 0:
+            precision = 0
+        else:
+            precision = true_pos/pred_pos
+        if real_pos == 0:
+            recall = 0
+        else:
+            recall =  true_pos/real_pos
+        if metric == 'precision':
+            return precision
+        elif metric == 'recall':
+            return recall
+        else:
+            if precision == recall == 0:
+                return 0
+            else:
+                return 2*precision*recall/(precision+recall)
